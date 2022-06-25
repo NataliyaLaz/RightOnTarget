1fbd8d9c87d2ded51b45c166867f761daeb73c5f
NataliyaLaz <nataliya.laz@gmail.com>
2022-06-25 10:25:19 +0300

    Initial Commit

---
+++ RightOnTarget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RightOnTarget
+//
+//  Created by Nataliya Lazouskaya on 25.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
